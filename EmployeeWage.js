6e61413be75c2d89b79e617c16e9cc7d6e35280b
Praful <prafulrakhade02@gmail.com>
2022-06-20 10:45:08 +0530

    [prafu;l] Added : Employee Wage Program file by using JavaScript

---
+++ EmployeeWage.js
@@ -0,0 +1,93 @@
+// uC 1 If else condition
+
+{
+    const IS_ABSENT=0;
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if(empCheck == IS_ABSENT)
+    {
+        console.log("UC1 - Employee is absent");
+        return;
+    }
+    else
+    {
+        console.log("UC1- Employee is present");
+    }
+}
+
+// UC 2 Switch Case
+
+const IS_PART_TIME=1;
+const IS_FULL_TIME=2;
+const PART_TIME_HOURS=4;
+const FULL_TIME_HOURS=8;
+const WAGE_PER_HOUR=20;
+let empHrs = 0
+{
+    
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    switch(empCheck)
+    {
+       case IS_PART_TIME:
+            empHrs=PART_TIME_HOURS;
+            break;
+        case IS_FULL_TIME:
+            empHrs=FULL_TIME_HOURS;
+            break;
+        default:
+            empHrs=0;
+            break;
+    }
+    let empWage = (empHrs * WAGE_PER_HOUR);
+    console.log("UC2 - Employee Wage : " + empWage);
+}
+// UC 3 Function
+
+{
+    let empCheck = Math.floor(Math.random() * 10) % 3;
+    function getWorkingHours(empCheck)
+    {
+        switch (empCheck)
+        {
+            case IS_PART_TIME:
+                return PART_TIME_HOURS;
+            case IS_FULL_TIME:
+                return FULL_TIME_HOURS;
+            default:
+                return 0;
+        }
+    }
+    empHrs = getWorkingHours(empCheck);
+    let empWage = empHrs * WAGE_PER_HOUR;
+    console.log("UC3 -  " + "Hour : " + empHrs + " Employee Wage : " + empWage);
+}
+
+// UC 4 For loop (repeatation Statement)
+
+{
+    const numOfWorkinfDays=20;
+    let totalEmpHrs=0;
+    for(let day=0; day<numOfWorkinfDays; day++)
+    {
+        let empCheck = Math.floor(Math.random() * 10 ) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs * WAGE_PER_HOUR;
+    console.log("UC4 - Total Hrs : " + totalEmpHrs + " Emp Wage : " + empWage);
+}
+
+// UC 5 While Loop (Repeatation statement)
+
+{
+    numOfWorkinfDays =20;
+    let maxHrsInMonth=160;
+    let totalEmpHrs=0;
+    let totaWorkingDays=0;
+    while(totalEmpHrs <= maxHrsInMonth && totaWorkingDays < numOfWorkinfDays)
+    {
+        totaWorkingDays++;
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+        totalEmpHrs += getWorkingHours(empCheck);
+    }
+    let empWage = totalEmpHrs *WAGE_PER_HOUR;
+    console.log(" UC5 - Total Days : " + totaWorkingDays + " Total Hrs : " + totalEmpHrs + " Emp Wage : " + empWage);
+}
